d6cbaee8f5f53e22f983fd97d0e9a1af3952c243
x2007tro <kemin.business@gmail.com>
2018-09-11 14:36:35 -0300

    error adj

--- app.R
+++ app.R
@@ -2,9 +2,9 @@
 # Source server and ui components
 ##
 source("global.R", local = FALSE)
+source("helper/shypka.R")
 source("iUI/main.R")
 source("iServer/main.R")
-source("helper/shypka.R")
 
 ##
 # Launch shiny app
